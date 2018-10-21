13f7964f286629bfd12e4afd19ed2ef3797b502d
icylisper <icylisper@gmail.com>
2018-10-21 02:26:17 -0700

    Add log/search-streams
    Acked-by: icylisper <icylisper@gmail.com>

--- src/sherlog/log.clj
+++ src/sherlog/log.clj
@@ -49,6 +49,17 @@
        (.filterLogEvents (get-client))
        (as-events)))
 
+(defn filter-log* [log-group log-streams pattern start-time token]
+  (->> (doto (FilterLogEventsRequest.)
+         (.withFilterPattern pattern)
+         (.withLogGroupName log-group)
+         (.withLogStreamNames log-streams)
+         (.withStartTime start-time)
+         (.withNextToken token)
+         (.withInterleaved true))
+       (.filterLogEvents (get-client))
+       (as-events)))
+
 (defn latest-log-stream [log-group]
   (->> (doto (DescribeLogStreamsRequest.)
         (.withOrderBy (OrderBy/valueOf "LastEventTime"))
@@ -74,13 +85,23 @@
   ([group stream events token]
    (lazy-seq (concat events (log-seq group stream token)))))
 
-(defn search [log-group pattern duration]
+(defn search-group [log-group pattern duration]
   (let [start (u/now-minus-secs duration)]
     (loop [{:keys [token events]} (filter-log log-group pattern start nil)
            acc []]
       (if-not token
         (conj acc events)
         (recur (filter-log log-group pattern start token)
+               (conj acc events))))))
+
+(defn search-streams [log-group streams pattern duration]
+  (let [start (u/now-minus-secs duration)]
+    (loop [{:keys [token events]}
+           (filter-log* log-group streams pattern start nil)
+           acc []]
+      (if-not token
+        (conj acc events)
+        (recur (filter-log* log-group streams pattern start token)
                (conj acc events))))))
 
 (defn init! [config]
