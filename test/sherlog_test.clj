3511e58c7f163ad3e9fa6184d1816e8161e0f4ca
icylisper <icylisper@gmail.com>
2018-12-30 17:35:01 -0800

    Add log-search test
    - rename log.publisher to log.input

--- test/sherlog_test.clj
+++ test/sherlog_test.clj
@@ -1,8 +1,10 @@
 (ns sherlog-test
   (:require
    [clojure.test :refer :all]
+   [cheshire.core :as json]
    [sherlog.core :as s]
-   [sherlog.util :as u]))
+   [sherlog.util :as u]
+   [sherlog.log.input :as in]))
 
 (def auth {:auth-type :profile :profile :prod-core})
 (def test-bucket (System/getenv "S3_TEST_BUCKET"))
@@ -24,6 +26,22 @@
                (first)
                (select-keys [:id :context]))))))
 
-(deftest ^:integration log-filter-test
+(deftest ^:integration log-search-test
   (s/init! auth :log)
-  (s/init! auth :metric))
+  (let [log-group "sherlog-test"
+        log-stream "test-stream"
+        msg        {:a "foo"
+                    :b "bar"
+                    :c {:d "baz"}
+                    :rand (rand-int 10)}]
+    (in/create-log-group log-group)
+    (in/create-log-stream log-group log-stream)
+    (->> (json/generate-string msg)
+         (in/put-event log-group log-stream))
+    (Thread/sleep 4000)
+    (is (= msg
+           (-> (s/search log-group {:a "foo"} 5)
+               (flatten)
+               (u/deserialize)
+               (first))))
+    (in/delete-log-group log-group)))
