6ee862fa51fde9f5840687352b07900b4fb4afd3
icylisper <icylisper@gmail.com>
2019-01-01 13:53:05 -0800

    Add simple bin/alarms script

--- src/sherlog/main.clj
+++ src/sherlog/main.clj
@@ -1,14 +1,16 @@
 (ns sherlog.main
   (:require
+   [clojure.pprint :as pprint]
    [sherlog.log :as log]
    [sherlog.s3 :as s3]
-   [sherlog.metric :as metric]))
+   [sherlog.metric :as metric]
+   [sherlog.log-alarm :as la]))
 
 (defn -main [& args]
   (let [[cmd log-group pattern duration & opts] args]
-    (log/init! {:auth-type :profile
-                :profile   (System/getenv "AWS_PROFILE")
-                :region    "us-east-1"})
+    (la/init! {:auth-type :profile
+               :profile   (System/getenv "AWS_PROFILE")
+               :region    "us-east-1"})
     (condp = (keyword cmd)
       :tail
       (doseq [line (log/tail log-group)]
@@ -18,4 +20,8 @@
       (doseq [line (->> (read-string duration)
                         (log/search log-group pattern)
                         (flatten))]
-        (println line)))))
+        (println line))
+
+      :alarms
+      (pprint/print-table
+       (la/list log-group)))))
